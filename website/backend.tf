1ce5c14dcda17672d50feae8f44098352b615a28
digitalden3 <denizyilmaz@hotmail.co.uk>
2023-09-29 23:04:40 +0100

    workflow

--- website/backend.tf
+++ website/backend.tf
@@ -1,8 +1,8 @@
 # Store the Terraform State File in S3 and Lock with DynamoDB
-#terraform {
- # backend "s3" {
-#    bucket         = "digitalden-terraform-tfstate2"
- #   key            = "default-infrastructure"
- #   region         = "us-east-1"
- ## }
-#}
+terraform {
+  backend "s3" {
+    bucket         = "digitalden-terraform-tfstate2"
+    key            = "default-infrastructure"
+    region         = "us-east-1"
+  }
+}
